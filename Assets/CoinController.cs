3550a599b73e5ff8d27047014a2a3e6b2ebf3f7f
mariaclarardrges-maker <225278544+mariaclarardrges-maker@users.noreply.github.com>
2026-06-19 08:58:06 -0300

    Aula 19/06 floresta

--- Assets/CoinController.cs
+++ Assets/CoinController.cs
@@ -6,7 +6,10 @@ public class CoinController : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player")){
+        if (other.CompareTag("Player"))
+        {
+            Point point = other.GetComponent<Point>();
+            point.AddPoints();
             Destroy(this.gameObject);
         }
     }
